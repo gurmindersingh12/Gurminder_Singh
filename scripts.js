2baabcc4aa5ec5aacd1cb478389fe958846795c5
Gurminder Singh <123406976+gurmindersingh12@users.noreply.github.com>
2024-07-02 15:17:21 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,12 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const li = document.createElement("li");
         li.innerHTML = `
             <div class="publication-title">
-                ${pub.title} <a href="${pub.url}" style="color: #1a0dab;">[Link]</a>
-                <div class="authors">${pub.authors}</div>
-            </div>
-            <div class="cited-by-year">
-                <div class="cited-by">Cited by ${pub.citations}</div>
-                <div class="year">${pub.year}</div>
+                ${pub.authors} ${pub.year}. ${pub.title} ${pub.journal}. (Cited By: ${pub.citations}) <a href="${pub.url}" style="color: #1a0dab;">[Link]</a>
             </div>
         `;
         publicationsList.appendChild(li);
@@ -27,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
             labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
             datasets: [{
                 label: 'Citations per Year',
-                data: [0, 1, 1, 1, 7, 2], // Example data, replace with actual citation data
+                data: [0, 1, 1, 1, 6, 2], // Example data, replace with actual citation data
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
