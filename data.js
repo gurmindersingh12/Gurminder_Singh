ce69bf72a7e8de71b34a3db3e305ea1db542011c
Gurminder Singh <123406976+gurmindersingh12@users.noreply.github.com>
2024-09-14 18:04:38 -0500

    Update data.js

--- data.js
+++ data.js
@@ -27,7 +27,22 @@ const publications = [
     citations: 2,
     url: "https://apsjournals.apsnet.org/doi/abs/10.1094/PDIS-03-23-0590-RE",
     abstract: "<strong>Abstract:</strong> Root-lesion nematode (RLN; Pratylenchus neglectus) is a migratory endoparasite and a major soilborne pathogen that affects wheat (<i>Triticum</i> spp.) production worldwide. Genetic resistance is one of the most economical and effective ways to manage P. neglectus in wheat. This study evaluated 37 local cultivars and germplasm lines in seven greenhouse experiments, including 26 hexaploid wheat, six durum wheat, two synthetic hexaploid wheat, one emmer wheat, and two triticale for P. neglectus resistance from 2016 to 2020. North Dakota field soils infested with two RLN populations (350 to 1,125 nematodes per kilogram of soil) were used for resistance screening under controlled greenhouse conditions. The final nematode population density for each cultivar and line was counted under the microscope to categorize the resistance ranking of these entries as resistant, moderately resistant, moderately susceptible, and susceptible. Out of the 37 cultivars and lines, one was classified as resistant (Brennan); 18 were moderately resistant (Divide, Carpio, Prosper, Advance, Alkabo, SY Soren, Barlow, Bolles, Select, Faller, Briggs, WB Mayville, SY Ingmar, W7984, PI 626573, Ben, Grandin, and Villax St. Jose); 11 were moderately susceptible; and seven were susceptible to P. neglectus. The resistant to moderately resistant lines identified in this study could be used in breeding programs after the resistance genes or loci are further elucidated. This research provides valuable information about P. neglectus resistance among wheat and triticale cultivars used in the Upper Midwest region of the United States.",
-    citingPapers: []
+    citingPapers: [
+      {
+      title: "Reproduction and molecular detection of plant-parasitic nematodes associated with potato.",
+      authors: "Goraya M",
+      journal: "<i>MS Thesis, North Dakota State University, Fargo, ND, USA</i>",
+      year: 2023,
+      url: "https://www.proquest.com/openview/7d141f2e1f4a10955286fc9bf10beab4/1?pq-origsite=gscholar&cbl=18750&diss=y"
+    },
+    {
+      title: "Molecular characterization of pin nematodes and evaluation of resistance in field pea cultivars",
+      authors: "Ojha E",
+      journal: "<i>MS Thesis, North Dakota State University, Fargo, ND, USA</i>",
+      year: 2023,
+      url: "https://www.proquest.com/openview/d20a446716a376dad835851bf2959cf6/1?pq-origsite=gscholar&cbl=18750&diss=y"
+    }
+    ]
   },
   {
     title: "Association mapping of resistance to tan spot in the Global Durum Panel.",
@@ -37,19 +52,39 @@ const publications = [
     citations: 3,
     url: "https://apsjournals.apsnet.org/doi/abs/10.1094/PHYTO-02-23-0043-R",
     abstract: "<strong>Abstract:</strong> Tan spot, caused by the necrotrophic fungal pathogen Pyrenophora tritici-repentis (Ptr), is an important disease of durum and common wheat worldwide. Compared with common wheat, less is known about the genetics and molecular basis of tan spot resistance in durum wheat. We evaluated 510 durum lines from the Global Durum Wheat Panel (GDP) for sensitivity to the necrotrophic effectors (NEs) Ptr ToxA and Ptr ToxB and for reaction to Ptr isolates representing races 1 to 5. Overall, susceptible durum lines were most prevalent in South Asia, the Middle East, and North Africa. Genome-wide association analysis showed that the resistance locus Tsr7 was significantly associated with tan spot caused by races 2 and 3, but not races 1, 4, or 5. The NE sensitivity genes Tsc1 and Tsc2 were associated with susceptibility to Ptr ToxC- and Ptr ToxB-producing isolates, respectively, but Tsn1 was not associated with tan spot caused by Ptr ToxA-producing isolates, which further validates that the Tsn1−Ptr ToxA interaction does not play a significant role in tan spot development in durum. A unique locus on chromosome arm 2AS was associated with tan spot caused by race 4, a race once considered avirulent. A novel trait characterized by expanding chlorosis leading to increased disease severity caused by the Ptr ToxB-producing race 5 isolate DW5 was identified, and this trait was governed by a locus on chromosome 5B. We recommend that durum breeders select resistance alleles at the Tsr7, Tsc1, Tsc2, and the chromosome 2AS loci to obtain broad resistance to tan spot.",
-    citingPapers: []
+    citingPapers: [
+      {
+      title: "Evolution, diversity, and function of the disease susceptibility gene <em>Snn1</em> in wheat.",
+      authors: "Seneviratne S, Shi G, Szabo-Hever A, Zhang Z, Haugrud A, Running K, <strong>Singh G</strong>, Nandety R, Fiedler J, McClean PE, Xu SS, Friesen TL, Faris JD",
+      journal: "<em>The Plant Journal</em>",
+      year: 2024,
+      url: "https://doi.org/10.1111/tpj.16879"
+    },
+    {
+      title: "Association mapping of tan spot and septoria nodorum blotch resistance in cultivated emmer wheat.",
+      authors: "Dhondup Lhamo, Qun Sun, Timothy L. Friesen, Anil Karmacharya, Xuehui Li, Jason D. Fiedler, Justin D. Faris, Guangmin Xia, Mingcheng Luo, Yong-Qiang Gu, Zhaohui Liu, Steven S. Xu",
+      journal: "<em>Theoretical and Applied Genetics</em>",
+      year: 2024,
+      url: "https://doi.org/10.1007/s00122-024-04700-2"
+    },
+    {
+      title: "Genome-wide association mapping of resistance to the foliar diseases septoria nodorum blotch and tan spot in a global winter wheat collection.",
+      authors: "Haugrud A, Shi G, Seneviratne S, Running K, Zhang Z, <strong>Singh G</strong>, Szabo-Hever A, Acharya K, Friesen TL, Liu Z, Faris JD",
+      journal: "<em>Molecular Breeding</em>",
+      year: 2023,
+      url: "https://doi.org/10.1007/s11032-023-01400-5"
+    }
+    ]
   },
   {
     title: "Genome-wide association mapping of resistance to the foliar diseases septoria nodorum blotch and tan spot in a global winter wheat collection.",
     authors: "Haugrud A, Shi G, Seneviratne S, Running K, Zhang Z, <strong>Singh G</strong>, Szabo-Hever A, Acharya K, Friesen TL, Liu Z, and Faris JD",
     journal: "<i>Molecular Breeding</i> 43:54",
     year: 2023,
-    citations: 4,
+    citations: 5,
     url: "https://link.springer.com/article/10.1007/s11032-023-01400-5",
     abstract: "<strong>Abstract:</strong> Septoria nodorum blotch (SNB) and tan spot, caused by the necrotrophic fungal pathogens Parastagonospora nodorum and Pyrenophora tritici-repentis, respectively, often occur together as a leaf spotting disease complex on wheat (Triticum aestivum L.). Both pathogens produce necrotrophic effectors (NEs) that contribute to the development of disease. Here, genome-wide association analysis of a diverse panel of 264 winter wheat lines revealed novel loci on chromosomes 5A and 5B associated with sensitivity to the NEs SnTox3 and SnTox5 in addition to the known sensitivity genes for NEs Ptr/SnToxA, SnTox1, SnTox3, and SnTox5. Sensitivity loci for SnTox267 and Ptr ToxB were not detected. Evaluation of the panel with five P. nodorum isolates for SNB development indicated the Snn3-SnTox3 and Tsn1-SnToxA interactions played significant roles in disease development along with additional QTL on chromosomes 2A and 2D, which may correspond to the Snn7-SnTox267 interaction. For tan spot, the Tsc1-Ptr ToxC interaction was associated with disease caused by two isolates, and a novel QTL on chromosome 7D was associated with a third isolate. The Tsn1-ToxA interaction was associated with SNB but not tan spot. Therefore some, but not all, of the previously characterized host gene-NE interactions in these pathosystems play significant roles in disease development in winter wheat. Based on these results, breeders should prioritize the selection of resistance alleles at the Tsc1, Tsn1, Snn3, and Snn7 loci as well as the 2A and 7D QTL to obtain good levels of resistance to SNB and tan spot in winter wheat.",
     citingPapers: [
-      // Existing citing papers (if any)
-      // Add the new citing papers below
       {
         title: "Intelligent reprogramming of wheat for enhancement of fungal and nematode disease resistance using advanced molecular techniques.",
         authors: "Jabran M, Ali MA, Zahoor A, Muhae-Ud-Din G, Liu T, Chen W, Gao L",
